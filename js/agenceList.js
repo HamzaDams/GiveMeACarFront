ed3fb6398cc2af65ed39d487926544fe96bb8c44
HamzaDams <hddesignx@gmail.com>
2020-12-02 09:53:20 +0100

    Refactor

---
+++ js/agenceList.js
@@ -0,0 +1,28 @@
+afficherAgences();
+
+function afficherAgences() {
+    fetch('http://localhost:8080/agences')
+    .then(response => response.json())
+    .then(data => {
+        let tableInfo = "";
+        data.forEach(element => {
+            tableInfo+=`
+            <tr>
+                <td class="grey">${element.id}</td>
+                <td class="white">${element.nom}</td>
+                <td class="grey">${element.ville}</td>
+                <td class="white">${element.nomDirecteur}</td>
+                <td class="grey">${element.adresse}</td>
+                <td class="white">${element.telephone}</td>
+                <td class="grey">${element.mail}</td>
+                <td class="white"><button onclick='navigation(${element.id})'>Voir</button></td>
+            </tr>
+        `
+        })
+        document.querySelector('#listTable').innerHTML = tableInfo;
+    });
+}
+
+function navigation(parcVehiculeId){
+    window.location.href = '/ajouteragence.html?id='+parcVehiculeId;
+}
